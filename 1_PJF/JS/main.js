9c0f1dda6ba7d8d40b486e036d973eab245643ef
REN <ren.mvmix@gmail.com>
2026-08-24 06:55:04 +0900

    Works modal animation added

--- 1_PJF/JS/main.js
+++ 1_PJF/JS/main.js
@@ -1,4 +1,4 @@
-/*hannburger menu*/
+/*hamburger menu*/
 
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
@@ -34,4 +34,31 @@ priceTitles.forEach(function (title) {
          const priceItem = title.parentElement;
          priceItem.classList.toggle("active");
       });
+});
+
+const worksButtons = document.querySelectorAll(".works-button");
+const worksModal = document.getElementById("works-Modal");
+const worksVideo = document.getElementById("works-Video");
+const worksClose = document.getElementById("works-close");
+
+worksButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+         const videoUrl = button.dataset.video;
+         const videoId = videoUrl.split("youtu.be/")[1].split("?")[0];
+         const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+         worksVideo.src = embedUrl;
+         worksModal.classList.add("active");
+      });
+});
+
+worksClose.addEventListener("click", function () {
+      worksModal.classList.remove("active");
+      worksVideo.src = "";
+});
+
+worksModal.addEventListener("click", function (e) {
+      if (e.target === worksModal) {
+         worksModal.classList.remove("active");
+         worksVideo.src = "";
+      }
 });
