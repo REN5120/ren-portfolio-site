22696d127b4966e3db27878efa3996957574c7f3
REN <ren.mvmix@gmail.com>
2026-08-24 07:17:18 +0900

    Works modal detail & Contact google marge added

--- 1_PJF/JS/main.js
+++ 1_PJF/JS/main.js
@@ -36,10 +36,11 @@ priceTitles.forEach(function (title) {
       });
 });
 
+// works modal
 const worksButtons = document.querySelectorAll(".works-button");
-const worksModal = document.getElementById("works-Modal");
-const worksVideo = document.getElementById("works-Video");
-const worksClose = document.getElementById("works-close");
+const worksModal = document.getElementById("worksModal");
+const worksVideo = document.getElementById("worksModalVideo");
+const worksClose = document.getElementById("worksModalClose");
 
 worksButtons.forEach(function (button) {
       button.addEventListener("click", function () {
@@ -47,6 +48,17 @@ worksButtons.forEach(function (button) {
          const videoId = videoUrl.split("youtu.be/")[1].split("?")[0];
          const embedUrl = `https://www.youtube.com/embed/${videoId}`;
          worksVideo.src = embedUrl;
+
+         const title = button.dataset.title;
+         const artist = button.dataset.artist;
+         const type = button.dataset.type;
+         const concept = button.dataset.concept;
+
+         document.getElementById("worksModalTitle").textContent = title;
+         document.getElementById("worksModalArtist").textContent = artist;
+         document.getElementById("worksModalType").textContent = type;
+         document.getElementById("worksModalConcept").textContent = concept;
+         
          worksModal.classList.add("active");
       });
 });
