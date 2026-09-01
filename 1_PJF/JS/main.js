abbbc2a394a1865b686de75bfdbf7920ca4a4fdb
REN <ren.mvmix@gmail.com>
2026-09-02 05:53:11 +0900

    header&footerのJS修正

--- 1_PJF/JS/main.js
+++ 1_PJF/JS/main.js
@@ -1,30 +1,54 @@
+// Header
+fetch("/1_PJF/components/header.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("header").innerHTML = data;
+
+    setupHamburger();
+});
+
+
+// Footer
+fetch("/1_PJF/components/footer.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+
 /*hamburger menu*/
 
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("nav");
+function setupHamburger() {
 
-hamburger.addEventListener("click", function () {
-    nav.classList.toggle("active");
-    hamburger.classList.toggle("active");
-});
+      const hamburger = document.getElementById("hamburger");
+      const nav = document.getElementById("nav");
 
-const navLinks = document.querySelectorAll(".nav a");
-
-navLinks.forEach(function (link) {
-      link.addEventListener("click", function (e) {
-            e.preventDefault();
-
-            const targetId = link.getAttribute("href");
-            const target = document.querySelector(targetId);
-
-            target.scrollIntoView({
-               behavior: "smooth"
-            });
-         
-         nav.classList.remove("active");
-         hamburger.classList.remove("active");
+      hamburger.addEventListener("click", function () {
+      nav.classList.toggle("active");
+      hamburger.classList.toggle("active");
       });
-});
+
+      const navLinks = document.querySelectorAll(".nav a");
+
+      navLinks.forEach(function (link) {
+            link.addEventListener("click", function (e) {
+                  e.preventDefault();
+
+                  const targetId = link.getAttribute("href");
+
+                  if(targetId.startsWith("/#")){
+                  window.location.href = targetId;
+                  }else{
+                  const target = document.querySelector(targetId);
+                  target.scrollIntoView({
+                        behavior:"smooth"
+                        });
+                  }
+                  
+            nav.classList.remove("active");
+            hamburger.classList.remove("active");
+            });
+      });
+}
 
 // price Accordion
 const accordionTitles = document.querySelectorAll(".accordion-title");
@@ -73,19 +97,4 @@ modal.addEventListener("click", function (e) {
          modal.classList.remove("active");
          modalVideo.src = "";
       }
-});
-
-// Header
-fetch("/1_PJF/components/header.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("header").innerHTML = data;
-});
-
-
-// Footer
-fetch("/1_PJF/components/footer.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("footer").innerHTML = data;
 });
